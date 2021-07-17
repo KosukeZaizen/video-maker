cd96f7e94f413dc111aa611f306e78516b7e5dc7
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-07-17 23:54:21 +0900

    activate react

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true, // this line is very important as it allows us to use `require` syntax in our html file.
+      contextIsolation: false,
     },
   })
   mainWindow.loadFile(`index.html`)
