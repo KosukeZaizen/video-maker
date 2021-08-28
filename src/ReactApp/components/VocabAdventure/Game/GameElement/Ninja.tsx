23bc78b0404383da454f2f57880240c98fc088da
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-08-28 17:41:23 +0900

    ninja gravity

--- src/ReactApp/components/VocabAdventure/Game/GameElement/Ninja.tsx
+++ src/ReactApp/components/VocabAdventure/Game/GameElement/Ninja.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
+import { useMemo } from "react";
 import { GameElement } from ".";
 import { staticFolderPath } from "../../../../common/consts";
 import { gameState } from "../GameState";
 
+const imgPath = `${staticFolderPath}/img/running_ninja.png`;
+
 export class Ninja extends GameElement {
+    private speedX = 0;
+    private speedY = 0;
+
     constructor(x: number, y: number, width: number) {
         super("ninja", x, y, width);
     }
@@ -12,24 +18,28 @@ export class Ninja extends GameElement {
         const { UL } = gameState;
         const { jump, goLeft, goRight } = gameState.command;
 
-        this.y += 1 * UL;
+        this.speedY += 0.02;
+        this.y += this.speedY * UL;
     };
 
     renderElement = () => {
         const { UL, timeStep } = gameState;
-        return (
-            <img
-                src={`${staticFolderPath}/img/running_ninja.png`}
-                style={{
+
+        const style = useMemo(
+            () =>
+                ({
                     willChange: "left, top",
-                    transitionProperty: "left top",
+                    transitionProperty: "left, top",
                     transitionDuration: `${timeStep}ms`,
+                    transitionTimingFunction: "linear",
                     position: "absolute",
                     left: this.x * UL,
                     top: this.y * UL,
                     width: this.width * UL,
-                }}
-            />
+                } as const),
+            [UL, this.x, this.y]
         );
+
+        return <img src={imgPath} style={style} />;
     };
 }
